3500f539ab5358fd32ba33851795f1272d0a4f6e
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2025-04-10 12:49:04 +0000

    change api to a free version

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const views = {
+const DOM = {
   crypto_converter_form: document.getElementById("crypto-converter-form"),
 
   amount_dropdown: document.getElementById("amount-dropdown"),
@@ -7,41 +7,67 @@ const views = {
 };
 
 window.addEventListener("load", async () => {
-  // get the list of crypto currencies
-  let crypto_currencies = await getCryptoCurrencies();
+  // get the lists of currencies
+  let crypto_currencies = await getCurrencies("/crypto-symbols");
+  let fiat_currencies = await getCurrencies("/fiat-symbols"); //regular currency- ie: USD
 
   // display data from the API on the page
-  renderView(crypto_currencies);
+  renderView(crypto_currencies, fiat_currencies);
+
+  // monitor form for changes in fields
+  DOM.crypto_converter_form.addEventListener("change", async (e) => {
+    let target_ele = e.target;
+
+    let amount_currency =
+      target_ele.id == "amount-dropdown" ? target_ele.value : null;
+
+    let amount_to_convert =
+      target_ele.id == "amount-to-be-converted" ? target_ele.value : null;
+
+    let currency_to_convert_to =
+      target_ele.id == "convert-to-dropdown" ? target_ele.value : null;
+
+    let date = target_ele.id == "date" ? target_ele.value : null;
+
+    console.log(
+      amount_currency,
+      amount_to_convert,
+      currency_to_convert_to,
+      date
+    );
+
+    // http://api.coinlayer.com/convert?access_key=8917ef8540765aaf5d1f0fa451b332e0&from=BTC&to=ETH&amount=10
+    let data = await fetchData(
+      "https://fast-price-exchange-rates.p.rapidapi.com/api/v1/convert?amount=1&base_currency=EUR&quote_currency=USD%2CBTC"
+    );
+
+    console.log("data", data);
+  });
 });
 
 // get a list of all crypto currencies
-async function getCryptoCurrencies() {
-  let url = `${CONFIG.baseURL}/list?access_key=${CONFIG.apiKey}`;
-  const crypto_currencies = await fetchData(url);
+async function getCurrencies(api_endpoint) {
+  let url = `${CONFIG.baseURL}${api_endpoint}`;
+  const result = await fetchData(url);
 
-  return Object.entries(crypto_currencies.crypto);
+  return Object.keys(result);
+  return result;
 }
 
 // display dynamic data in DOM
-function renderView(crypto_list) {
+function renderView(crypto_currencies, fiat_currencies) {
   // display crypto currencies for amount dropdown field in the converter form
-  displayDropdownItems(crypto_list, views.amount_dropdown);
+  displayDropdownItems(crypto_currencies, DOM.amount_dropdown);
 
   // display crypto currencies for convert to dropdown field in the converter form
-  displayDropdownItems(crypto_list, views.convert_to_dropdown);
+  displayDropdownItems(fiat_currencies, DOM.convert_to_dropdown);
 }
 
 async function displayDropdownItems(crypto_list, DOM_node) {
   // loop through crypto_list and create a new option element for each items in the list
   let dropdown_items = crypto_list.map((crypto, index) => {
-    // check if crypto list is being displayed in the amount dropdown element
-    let isDisplayingAmountDropdown =
-      index == 2 && DOM_node.id === "amount-dropdown";
-
     // conditionally set select attribute
-    let html = isDisplayingAmountDropdown
-      ? `<option value=${crypto[1].symbol} selected> ${crypto[1].symbol}</option>`
-      : `<option value=${crypto[1].symbol}> ${crypto[1].symbol}</option>`;
+    let html = `<option value=${crypto}> ${crypto}</option>`;
 
     return html;
   });
@@ -54,7 +80,12 @@ async function displayDropdownItems(crypto_list, DOM_node) {
 async function fetchData(url) {
   const api_url = url;
 
-  const response = await fetch(url);
+  const response = await fetch(api_url, {
+    headers: {
+      "x-rapidapi-key": "8267f7a4e0mshf69609913b1eb22p11bd12jsn710ca9ddccf3",
+    },
+  });
+
   const data = await response.json();
 
   return data;
